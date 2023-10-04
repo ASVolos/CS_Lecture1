a621b904ed234801c92afc52c730f9f88b873561
ASV <samatovivan@mail.ru>
2023-10-04 11:21:46 +0500

    HelloConsole

---
+++ HelloConsole/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2 Hello, World!");
+// dotnet new console - создать новый проект;
+// dotnet run - запустить проект;
+// Console используем для взаимодействия с системный терминалом;
