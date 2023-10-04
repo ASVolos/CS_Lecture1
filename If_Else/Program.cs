213dae06022cfbf425d4b56a61ea6411a6c4127a
ASV <samatovivan@mail.ru>
2023-10-04 13:44:40 +0500

    if-else

---
+++ If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username);
+}
